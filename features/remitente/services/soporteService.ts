6ac095777ec172fb66b878213bc257ea6f21d6b3
Nicolas Bernardini <nicolasbernardini54@gmail.com>
2026-06-21 11:17:30 -0300

    feat: modificar backend para agregar el nombre del usuario de soporte

--- features/remitente/services/soporteService.ts
+++ features/remitente/services/soporteService.ts
@@ -3,7 +3,7 @@ import { TicketHistorial } from "../types/soporte";
 export const soporteService = {
   async obtenerHistorialTickets(): Promise<TicketHistorial[]> {
     const response = await fetch("/api/usuarios/me/tickets");
-    
+
     if (!response.ok) {
       console.error("Error fetching tickets", response.statusText);
       return [];
@@ -12,14 +12,14 @@ export const soporteService = {
     const json = await response.json();
     return json.data.map((ticket: any) => ({
       id: `TK-${ticket.codigoReclamo}`,
-      fecha: new Intl.DateTimeFormat('es-AR', {
-        day: '2-digit',
-        month: 'short',
-        year: 'numeric'
+      fecha: new Intl.DateTimeFormat("es-AR", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
       }).format(new Date(ticket.creadoEn)),
       asunto: ticket.motivo,
       envio_id: ticket.codigoSeguimiento.toString(),
-      soporte: ticket.dniSoporteTecnico || "Sin asignar",
+      soporte: ticket.nombreHelper || "Sin asignar",
       estado: ticket.estado,
     }));
   },
